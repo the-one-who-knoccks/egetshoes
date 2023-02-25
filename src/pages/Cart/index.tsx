0c949c02beb83aeefee4a6de36b9347cbb1190b0
the-one-who-knoccks <thiagoalves.devp>
2023-02-24 23:34:11 -0300

    :sparkles: Added new pages

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md'
+
+import { connect, DispatchProp } from 'react-redux'
+import ReturnType from 'typescript'
+import { Container, ProductTable, Total } from './styles'
+import { RootState } from '../../store/modules/rootReducer'
+import * as CartActions from '../../store/modules/cart/actions'
+import { IProduct } from '../../types'
+import { formatPrice } from '../../util/format'
+
+const mapStateToProps = (state: RootState) => ({
+  products: state.cart.products.map((product) => ({
+    ...product,
+    subtotal: formatPrice(product.amount * product.price),
+  })),
+  total: formatPrice(
+    state.cart.products.reduce((total, product) => {
+      return total + product.price * product.amount
+    }, 0),
+  ),
+})
+
+type StateProps = ReturnType<typeof mapStateToProps>
+
+type Props = StateProps & DispatchProp
+
+function Cart(props: Props) {
+  const { products, dispatch, total } = props
+
+  function incrementProduct(product: IProduct) {
+    dispatch(CartActions.updateProductAmount(product.id, product.amount + 1))
+  }
+
+  function decrementProduct(product: IProduct) {
+    dispatch(CartActions.updateProductAmount(product.id, product.amount - 1))
+  }
+
+  return (
+    <Container>
+      {products.map((product) => (
+        <ProductTable key={product.id}>
+          <thead>
+            <tr>
+              <th />
+              <th>Produto</th>
+              <th>QTDE</th>
+              <th>Subtotal</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img src={product.image} alt="PC gamer" />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+
+              <td>
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => decrementProduct(product)}
+                  >
+                    <MdRemoveCircleOutline size={20} color="#7159c2" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button
+                    type="button"
+                    onClick={() => incrementProduct(product)}
+                  >
+                    <MdAddCircleOutline size={20} color="#7159c2" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subtotal}</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => dispatch(CartActions.removeToCart(product.id))}
+                >
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </ProductTable>
+      ))}
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>{total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  )
+}
+export default connect(mapStateToProps)(Cart)
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { useAsyncEffect } from 'use-async-effect'
+import { connect, DispatchProp } from 'react-redux'
+import ReturnType from 'typescript'
+import { ProductList } from './styles'
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
+import { IProduct } from '../../types'
+
+import { RootState } from '../../store/modules/rootReducer'
+import * as CartActions from '../../store/modules/cart/actions'
+
+type amountProduct = { [key: number]: any }
+const amountObject: amountProduct = {}
+const mapStateToProps = (state: RootState) => ({
+  cart: state.cart.products,
+  amount: state.cart.products.reduce((amount, currentValue) => {
+    // eslint-disable-next-line no-param-reassign
+    amount[currentValue.id] = currentValue.amount
+    return amount
+  }, amountObject),
+})
+
+type StateProps = ReturnType<typeof mapStateToProps>
+
+type Props = StateProps & DispatchProp
+
+function Home(props: Props) {
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  useAsyncEffect(async () => {
+    const response = await api.get('products')
+
+    const productsData = response.data.map((product: IProduct) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    setProducts(productsData)
+  }, [])
+
+  function handleAddProduct(product: IProduct) {
+    const { dispatch } = props
+    dispatch(
+      CartActions.addProductToCart({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }),
+    )
+  }
+
+  return (
+    <ProductList>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button type="button" onClick={() => handleAddProduct(product)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />
+              {props.amount[product.id] || 0}
+            </div>
+            <span>Adicionar ao Carrinho</span>
+          </button>
+        </li>
+      ))}
+      )
+    </ProductList>
+  )
+}
+
+export default connect(mapStateToProps)(Home)
